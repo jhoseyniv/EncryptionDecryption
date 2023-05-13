54884cfe23dfbea182f912e0da9cc781f192a907
98912 <jhoseyniv@gmail.com>
2023-05-13 22:29:43 +0430

    first commit-version1

--- src/main/java/AsymetricRSA/Asymmetric.java
+++ src/main/java/AsymetricRSA/Asymmetric.java
@@ -1,8 +1,8 @@
 package AsymetricRSA;
 
-import java.security.KeyPair;
-import java.security.KeyPairGenerator;
-import java.security.SecureRandom;
+import javax.crypto.Cipher;
+import java.nio.charset.StandardCharsets;
+import java.security.*;
 
 public class Asymmetric {
     private static final String RSA = "RSA";
@@ -19,5 +19,20 @@ public class Asymmetric {
 
         return keyPairGenerator.generateKeyPair();
     }
+    public static byte[] doEncryption(String plainText,PrivateKey privateKey) throws Exception
+    {
+        Cipher cipher= Cipher.getInstance(RSA);
+        cipher.init(Cipher.ENCRYPT_MODE, privateKey);
+        byte[] encrypetedText = cipher.doFinal(plainText.getBytes(StandardCharsets.UTF_8));
+        return encrypetedText;
+    }
+    public static String doDecrypte(byte[] cipherText, PublicKey publicKey) throws Exception {
+        Cipher cipher= Cipher.getInstance(RSA);
+        cipher.init(Cipher.DECRYPT_MODE, publicKey);
+        byte[] decryptedText = cipher.doFinal(cipherText);
+        return new String(decryptedText);
+
+    }
+
 
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,7 +10,17 @@ public class Main {
         KeyPair keypair  = Asymmetric.generateRSAKkeyPair();
         System.out.println("A Key Pair Generated for Cryptography by RSA Algorithm... ");
         System.out.println("A Public Key is= "+ DatatypeConverter.printHexBinary(keypair.getPublic().getEncoded()));
-        System.out.println("A Priave Key is= "+ DatatypeConverter.printHexBinary(keypair.getPrivate().getEncoded()));
+        System.out.println("A Private Key is= "+ DatatypeConverter.printHexBinary(keypair.getPrivate().getEncoded()));
+
+
+        System.out.println("We Want to encrypt  and Decrypt 'Jalal Hosseini' with private and public key ... ");
+
+        byte[] encryptedText =  Asymmetric.doEncryption("Jalal Hosseini",keypair.getPrivate());
+        System.out.println("Encrypted Text for 'Jalal Hosseini' is = " + DatatypeConverter.printHexBinary(encryptedText));
+
+        String decryptedText =  Asymmetric.doDecrypte(encryptedText,keypair.getPublic());
+        System.out.println("Decryptd  Text for "+DatatypeConverter.printHexBinary(encryptedText) +" is = " + decryptedText);
+
 
     }
 }
